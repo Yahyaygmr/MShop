8997957ed64c4b54c3acd632934242ec2271bf5d
Yahyaygmr <yahyaygmr@gmail.com>
2024-10-11 17:08:52 +0300

    ui layout ve default sayfası tasarımı

---
+++ Frontends/MShop.WebUI/ViewComponents/UILayoutViewComponets/UILayoutFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.ViewComponents.UILayoutViewComponets
+{
+    public class UILayoutFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
