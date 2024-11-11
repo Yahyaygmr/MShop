9f8eb629bdf18c57d447d5abdf47adb21b3f153e
Yahyaygmr <yahyaygmr@gmail.com>
2024-11-11 15:46:14 +0300

    feature slider ui admin işlemleri tamamlandı

---
+++ Frontends/MShop.WebUI/Areas/Admin/Controllers/FeatureSliderController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MShop.DtoLayer.CatalogDtos.FeatureSliderDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/{controller}/{action}/{id?}")]
+    public class FeatureSliderController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FeatureSliderController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7205/api/FeatureSliders");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateFeatureSlider()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFeatureSlider(CreateFeatureSliderDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+
+            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7205/api/FeatureSliders", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteFeatureSlider([FromRoute] string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7205/api/FeatureSliders/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateFeatureSlider([FromRoute] string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7205/api/FeatureSliders/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateFeatureSliderDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateFeatureSlider(UpdateFeatureSliderDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+
+            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7205/api/FeatureSliders", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> ChangeFeatureSliderStatus([FromRoute] string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+           await client.GetAsync($"https://localhost:7205/api/FeatureSliders/ChangeFeatureSliderStatus/{id}");
+
+            return RedirectToAction("Index");
+        }
+    }
+}
