84c62192860eac5884df8c9a9a3d84271500f723
Yahyaygmr <yahyaygmr@gmail.com>
2024-11-13 12:32:42 +0300

    feature alanı ui işlemleri tamamlandı

--- Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/DefaultFeatureComponentPartial.cs
+++ Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/DefaultFeatureComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MShop.DtoLayer.CatalogDtos.FeatureDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class DefaultFeatureComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultFeatureComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7205/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
