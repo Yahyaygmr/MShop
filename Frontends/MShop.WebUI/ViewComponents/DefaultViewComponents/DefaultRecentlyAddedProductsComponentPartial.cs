9a2365fd086bc2e0923d3b2313cf8bc7ccbcf3c5
Yahyaygmr <yahyaygmr@gmail.com>
2024-11-13 14:20:43 +0300

    ui tarafı son eklenen ürünler listelendi

---
+++ Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/DefaultRecentlyAddedProductsComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace MShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class DefaultRecentlyAddedProductsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultRecentlyAddedProductsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7205/api/Products/ProductListWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
+                return View(values?.Take(8).ToList());
+            }
+            return View();
+        }
+    }
+}
