aee48fb2f0744905e5b646897d7fd08d1544b299
Yahyaygmr <yahyaygmr@gmail.com>
2024-11-13 15:43:09 +0300

    markaların ui da listelenmesi tamamlandı

---
+++ Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/DefaultBrandComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MShop.DtoLayer.CatalogDtos.BrandDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace MShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class DefaultBrandComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultBrandComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7205/api/Brands");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
