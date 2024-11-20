63c5e0825349f7bcee571073f0db892a1126104d
Yahyaygmr <yahyaygmr@gmail.com>
2024-11-20 14:12:06 +0300

    view ürün detay sayfasında ürün görsellerinin listelenmesi işlemi tamamlandı

--- Frontends/MShop.WebUI/ViewComponents/ProductDetailViewComponents/ProductDetailProductImagesComponentPartial.cs
+++ Frontends/MShop.WebUI/ViewComponents/ProductDetailViewComponents/ProductDetailProductImagesComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MShop.DtoLayer.CatalogDtos.ProductImageDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class ProductDetailProductImagesComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductDetailProductImagesComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            ViewBag.productId = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7205/api/ProductImages/ProductImageListByProductId/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductImageDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
