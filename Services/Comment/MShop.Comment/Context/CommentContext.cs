8f2e56646ae1d8c4c7533f3ec122df6806807733
Yahyaygmr <yahyaygmr@gmail.com>
2024-12-21 16:18:31 +0300

    uder comment api ve veritanı hazırlandı

---
+++ Services/Comment/MShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MShop.Comment.Entities;
+using System.Collections.Generic;
+
+namespace MShop.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1442;initial catalog=MShopCommentDb;User=sa;Password=123456aA*;TrustServerCertificate=true;");
+        }
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
