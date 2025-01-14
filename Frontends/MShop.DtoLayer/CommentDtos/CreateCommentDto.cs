c6530769f5fd8ae896477d9384832a0549f89da4
Yahyaygmr <yahyaygmr@gmail.com>
2025-01-14 15:50:45 +0300

    commente ait dtolar oluşturuldu

---
+++ Frontends/MShop.DtoLayer/CommentDtos/CreateCommentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MShop.DtoLayer.CommentDtos
+{
+    public class CreateCommentDto
+    {
+        public string? ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string CommentDetail { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Status { get; set; }
+        public string ProductId { get; set; }
+    }
+}
